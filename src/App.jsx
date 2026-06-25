c3db3394e17cdc5a602602224f645efc221dde59
mfhsohag01 <md.sohag2001bd@gmail.com>
2026-06-26 00:01:18 +0600

    feat: render movie cards using data.map() with sidebar navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import MovieList from "./cine/MovieList";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList />
+        </div>
+      </main>
     </div>
   );
 }
