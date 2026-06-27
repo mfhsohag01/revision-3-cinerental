220373283242aed9e82bff336fc41a9b62cd17d0
mfhsohag01 <md.sohag2001bd@gmail.com>
2026-06-27 07:34:16 +0600

    feat: add cart modal with remove and checkout functionality

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import Moon from "./assets/icons/moon.svg";
 import Logo from "./assets/logo.svg";
 import Ring from "./assets/ring.svg";
 import ShoppingCart from "./assets/shopping-cart.svg";
+import CardDetails from "./cine/CardDetails";
 
 export default function Header() {
+  const [showCart, setShowCart] = useState(false);
+
+  const handleCartShow = () => {
+    setShowCart(true);
+  };
   return (
     <div>
       <header>
+        {showCart && <CardDetails onClose={() => setShowCart(false)} />}
         <nav className="container flex items-center justify-between space-x-10 py-6">
           <a href="index.html">
             <img src={Logo} width="139" height="26" alt="Logo" />
@@ -33,6 +41,7 @@ export default function Header() {
               <a
                 className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
                 href="#"
+                onClick={handleCartShow}
               >
                 <img src={ShoppingCart} width="24" height="24" alt="" />
               </a>
